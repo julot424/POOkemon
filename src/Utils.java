11156670d8ba2ece38b4890ce3e5accef693d4a4
Jules Klausnitzer <klausnitzer@unista.fr>
2024-04-23 22:28:51 +0200

    Ajout de methodes pour la generation des pokemons

--- src/Utils.java
+++ src/Utils.java
@@ -1,6 +1,64 @@
 import java.lang.annotation.ElementType;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 public class Utils {
+
+    //ArrayList couleurs = new ArrayList<(Arrays.asList(Rouge, Bleu, Vert));
+    private static ArrayList listePokemon = new ArrayList(Arrays.asList(
+            "Bulbizarre", "Herbizarre", "Florizarre",
+            "Salamèche", "Reptincel", "Dracaufeu",
+            "Carapuce", "Carabaffe", "Tortank",
+            "Chenipan", "Chrysacier", "Papilusion",
+            "Aspicot", "Coconfort", "Dardargnan",
+            "Roucool", "Roucoups", "Roucarnage",
+            "Rattata", "Rattatac", "Piafabec",
+            "Rapasdepic", "Abo", "Arbok",
+            "Pikachu", "Raichu", "Sabelette",
+            "Sablaireau", "Nidoran♀", "Nidorina",
+            "Nidoqueen", "Nidoran♂", "Nidorino",
+            "Nidoking", "Mélofée", "Mélodelfe",
+            "Goupix", "Feunard", "Rondoudou",
+            "Grodoudou", "Nosferapti", "Nosferalto",
+            "Mystherbe", "Ortide", "Rafflesia",
+            "Paras", "Parasect", "Mimitoss",
+            "Aéromite", "Taupiqueur", "Triopikeur",
+            "Miaouss", "Persian", "Psykokwak",
+            "Akwakwak", "Férosinge", "Colossinge",
+            "Caninos", "Arcanin", "Ptitard",
+            "Têtarte", "Tartard", "Abra",
+            "Kadabra", "Alakazam", "Machoc",
+            "Machopeur", "Mackogneur", "Chétiflor",
+            "Boustiflor", "Empiflor", "Tentacool",
+            "Tentacruel", "Racaillou", "Gravalanch",
+            "Grolem", "Ponyta", "Galopa",
+            "Ramoloss", "Flagadoss", "Magnéti",
+            "Magnéton", "Canarticho", "Doduo",
+            "Dodrio", "Otaria", "Lamantine",
+            "Tadmorv", "Grotadmorv", "Kokiyas",
+            "Crustabri", "Fantominus", "Spectrum",
+            "Ectoplasma", "Onix", "Soporifik",
+            "Hypnomade", "Krabby", "Krabboss",
+            "Voltorbe", "Électrode", "Noeunoeuf",
+            "Noadkoko", "Osselait", "Ossatueur",
+            "Kicklee", "Tygnon", "Excelangue",
+            "Smogo", "Smogogo", "Rhinocorne",
+            "Rhinoféros", "Leveinard", "Saquedeneu",
+            "Kangourex", "Hypotrempe", "Hypocéan",
+            "Poissirène", "Poissoroy", "Stari",
+            "Staross", "M. Mime", "Insécateur",
+            "Lippoutou", "Élektek", "Magmar",
+            "Scarabrute", "Tauros", "Magicarpe",
+            "Léviator", "Lokhlass", "Métamorph",
+            "Évoli", "Aquali", "Voltali",
+            "Pyroli", "Porygon", "Amonita",
+            "Amonistar", "Kabuto", "Kabutops",
+            "Ptéra", "Ronflex", "Artikodin",
+            "Électhor", "Sulfura", "Minidraco",
+            "Draco", "Dracolosse", "Mewtwo",
+            "Mew"));
+
     private static final Random random = new Random();
 
     /**
@@ -31,5 +89,24 @@ public class Utils {
         Type[] types = Type.values();
         int index = random.nextInt(types.length);
         return types[index];
+    }
+
+    /**
+     *
+     * @return Un string et le supprime, le String et generer aléatoirement.
+     */
+    public static String getRandomNom()
+    {
+        int i = random.nextInt(listePokemon.size());
+        return listePokemon.remove(i).toString();
+    }
+
+    /**
+     * Sert au debuggage
+     * @return La taille du tableau des noms des pokemon.
+     */
+    public static int taille()
+    {
+        return listePokemon.size();
     }
 }
