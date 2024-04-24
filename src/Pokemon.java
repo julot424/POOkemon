e6855ca25652236aed5ce7bca76203de296c8eaf
letzy54 <152508016+letzy54@users.noreply.github.com>
2024-04-24 13:51:26 +0200

    UPDATE Pokemon, ne contient plus que ses attributs et constructeur

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,57 +1,7 @@
-import java.util.ArrayList;
-
 /**
  * La classe Pokemon permet de créer un Pokémon dans le jeu.
  */
 public class Pokemon {
-
-  /**
-   * La méthode principale du programme.
-   */
-  public static void main(String[] args) {
-    System.out.println("Lancement du jeu ...");
-    System.out.println("-------------------------------");
-
-    Joueur joueur = new Joueur();//création d'un nouveau joueur
-
-    // Ajout de trois Pokémon au deck du joueur
-    for (int i = 0; i < 3; i++) {
-      joueur.ajouterPokemon(new Pokemon(Utils.getRandomNom(), Utils.getRandomPV(), Utils.getRandomATK(), Utils.getRandomType()));
-    }
-
-    // Affichage du deck du joueur
-    afficherDeckJoueur(joueur);
-  }
-
-  /**
-   * Afficher le deck d'un joueur.
-   * @param joueur Le joueur dont le deck doit être affiché.
-   */
-  public static void afficherDeckJoueur(Joueur joueur) {
-    System.out.println("Deck du joueur:");
-    System.out.println("<-------------------------------->");
-    ArrayList<Pokemon> deckJoueur = joueur.getDeck();
-    for (int i = 0; i < deckJoueur.size(); i++) {
-      afficherDetailsPokemon("Pokémon " + (i + 1), deckJoueur.get(i));
-    }
-  }
-
-
-  /**
-   * Affiche les détails d'un Pokémon.
-   * @param titre Le titre du Pokémon.
-   * @param pokemon Le Pokémon dont les détails doivent être affichés.
-   */
-  public static void afficherDetailsPokemon(String titre, Pokemon pokemon) {
-    System.out.println(titre);
-    System.out.println("Pokémon: " + pokemon.m_nom);
-    System.out.println("Point de vie: " + pokemon.m_PV);
-    System.out.println("Attaque: " + pokemon.m_atk);
-    System.out.println("Type: " + pokemon.m_type);
-    System.out.println("-------------------------------");
-  }
-
-
   // Attributs
   String m_nom;
   int m_PV;
