e98d879845d0dfe375aa07e018aee5f659ec9bb4
letzy54 <152508016+letzy54@users.noreply.github.com>
2024-04-24 13:50:45 +0200

    ADD class Main, pour permettre de gérer tout les affichages

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Lancement du jeu ...");
+        System.out.println("-------------------------------");
+
+        Joueur joueur = new Joueur();
+
+        // Ajout de trois Pokémon au deck du joueur
+        for (int i = 0; i < 3; i++) {
+            joueur.ajouterPokemon(new Pokemon(Utils.getRandomNom(), Utils.getRandomPV(), Utils.getRandomATK(), Utils.getRandomType()));
+        }
+
+        // Affichage du deck du joueur
+        AffichageJoueur.afficherDeckJoueur(joueur);
+    }
+}
