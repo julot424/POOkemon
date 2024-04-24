1dc208bd4392e8e7e718d6ff7b962db0dc18bc6b
letzy54 <152508016+letzy54@users.noreply.github.com>
2024-04-24 13:32:41 +0200

    UPDATE Joueur, JAVADOC

--- src/Joueur.java
+++ src/Joueur.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
  * Classe représentant un joueur dans le jeu.
  */
 public class Joueur {
-    //Attributs
+    // Attributs
     private String m_nom;
     private ArrayList<Pokemon> m_deck;
+
+    /**
+     * Constructeur par défaut de la classe Joueur.
+     * Initialise le deck du joueur.
+     */
     public Joueur() {
         m_deck = new ArrayList<>(); // Initialisation du deck
     }
@@ -18,6 +23,7 @@ public class Joueur {
     public ArrayList<Pokemon> getDeck() {
         return this.m_deck;
     }
+
     /**
      * Ajoute un Pokémon au deck du joueur.
      * @param pokemon Le Pokémon à ajouter.
