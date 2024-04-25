86307a2cccf3fd10c26b43c17d9fc0cad533b3de
letzy54 <152508016+letzy54@users.noreply.github.com>
2024-04-25 11:43:08 +0200

    MAJ GLOBAL

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,43 +1,66 @@
-public class Pokemon
-{
+/**
+ * La classe Pokemon représente un Pokémon avec un nom, des points de vie (PV),
+ * une attaque (atk) et un type.
+ */
+public class Pokemon {
   String m_nom;
   int m_PV;
   int m_atk;
   Type m_type;
 
-  public Pokemon(String nom, int PV, int atk, Type type)
-  {
+  /**
+   * Constructeur de la classe Pokemon.
+   * @param nom Le nom du Pokémon.
+   * @param PV Les points de vie du Pokémon.
+   * @param atk La valeur de l'attaque du Pokémon.
+   * @param type Le type du Pokémon.
+   */
+  public Pokemon(String nom, int PV, int atk, Type type) {
     this.m_nom = nom;
     this.m_atk = atk;
     this.m_PV = PV;
     this.m_type = type;
   }
 
-  public boolean attaquer(Pokemon cible)
-  {
+  /**
+   * Méthode pour que le Pokémon attaque un autre Pokémon.
+   * @param cible Le Pokémon cible de l'attaque.
+   * @return Retourne true si l'attaque réussit.
+   */
+  public boolean attaquer(Pokemon cible) {
     cible.m_PV -= this.getAtk();
     return true;
   }
 
-  public String getNom()
-  {
+  /**
+   * Méthode pour obtenir le nom du Pokémon.
+   * @return Le nom du Pokémon.
+   */
+  public String getNom() {
     return this.m_nom;
   }
-  public int getAtk()
-  {
+
+  /**
+   * Méthode pour obtenir la valeur de l'attaque du Pokémon.
+   * @return La valeur de l'attaque du Pokémon.
+   */
+  public int getAtk() {
     return this.m_atk;
   }
 
-  public int getPV()
-  {
+  /**
+   * Méthode pour obtenir les points de vie du Pokémon.
+   * @return Les points de vie du Pokémon.
+   */
+  public int getPV() {
     return this.m_PV;
   }
 
-  public Type getType()
-  {
+  /**
+   * Méthode pour obtenir le type du Pokémon.
+   * @return Le type du Pokémon.
+   */
+  public Type getType() {
     return this.m_type;
   }
-
-
-
 }
