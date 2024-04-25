d912c03118a2e4ab96de30171a1e7e3d09f4c3af
Jules Klausnitzer <klausnitzer@unista.fr>
2024-04-25 23:48:28 +0200

    RENAME anciene class Main.java --> jeu.java

---
+++ src/Jeu.java
@@ -0,0 +1,55 @@
+import jdk.jshell.execution.Util;
+
+public class Jeu {
+
+    static Joueur user = new Joueur();
+    static Joueur ia = new Joueur();
+
+
+    public static void main(String[] args) {
+        System.out.println("Lancement du jeu ...");
+        System.out.println("-------------------------------");
+
+        if(Utils.getRandomFirstPlayer() == 0)
+        {
+            System.out.println("AI FIRST");
+            jeuAIFirst();
+        }
+        else
+        {
+            System.out.println("USER FIRST");
+            jeuUserFirst();
+        }
+
+        Affichage.afficherTailleDeck(ia);
+        Affichage.afficherTailleDeck(user);
+
+
+    }
+
+    public static void jeuAIFirst()
+    {
+        for(int i = 0; i < 20; i++)
+        {
+            ia.ajouterPokemon(new Pokemon(Utils.getRandomNom(), Utils.getRandomPV(), Utils.getRandomATK(), Utils.getRandomType()));
+        }
+
+        for(int i = 0; i < 21; i++)
+        {
+            user.ajouterPokemon(new Pokemon(Utils.getRandomNom(), Utils.getRandomPV(), Utils.getRandomATK(), Utils.getRandomType()));
+        }
+    }
+
+    public static void jeuUserFirst()
+    {
+        for(int i = 0; i < 20; i++)
+        {
+            user.ajouterPokemon(new Pokemon(Utils.getRandomNom(), Utils.getRandomPV(), Utils.getRandomATK(), Utils.getRandomType()));
+        }
+
+        for(int i = 0; i < 21; i++)
+        {
+            ia.ajouterPokemon(new Pokemon(Utils.getRandomNom(), Utils.getRandomPV(), Utils.getRandomATK(), Utils.getRandomType()));
+        }
+    }
+}
--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import jdk.jshell.execution.Util;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Lancement du jeu ...");
-        System.out.println("-------------------------------");
-
-    }
-}
